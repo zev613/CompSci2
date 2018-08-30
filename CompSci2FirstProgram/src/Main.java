098676158227f6bdb41a45c14b2b95c3b3b5bdec
zev613 <zev123@gmail.com>
2018-08-30 10:54:55 -0400

    CompSci2FirstProgram Add Branch

---
+++ CompSci2FirstProgram/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String myName = "Zev Seltzer";
+        int gradYear = 2020;
+        System.out.println("My name is " + myName + " and I'll graduate in " + gradYear + ".");
+    }
+}
